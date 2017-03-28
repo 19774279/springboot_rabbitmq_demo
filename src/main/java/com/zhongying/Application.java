849df9a4bfaca8a09ff0c193c37df29dd4e65b01
19774279 <19774279@qq.com>
2017-03-28 14:19:36 +0800

    springboot+rabbitmq

---
+++ src/main/java/com/zhongying/Application.java
@@ -0,0 +1,17 @@
+package com.zhongying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by zhusj on 2017/3/24.
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = "com.zhongying")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
